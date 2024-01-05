8a0292863a506782dacbfa58a9ac5f3edfba6681
Sir Guevara <groovyguev@gmail.com>
2024-01-04 19:21:27 -0600

    reverted to Typescript hono and bun

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from app.config import PORT
-
-# from app.main import app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app="app.main:app", host="127.0.0.1", port=int(PORT), reload=True)
