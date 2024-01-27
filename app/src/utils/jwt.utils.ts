7f8958bc9740f3ee281e332ac9bf4571ba118e34
Sir Guevara <groovyguev@gmail.com>
2024-01-27 00:57:40 -0600

    jwt verify

--- app/src/utils/jwt.utils.ts
+++ app/src/utils/jwt.utils.ts
@@ -1,37 +1,27 @@
- import {sign, verify } from 'hono/jwt'
- 
+import { sign, verify } from "hono/jwt";
 
- const JWT_SECRET = Bun.env.JWT_SECRET;
- const ALGO = Bun.env.ALGORITHM as "HS256"|"HS384"|"HS512";
- export const signJwt = async (payload:{ [key: string]: unknown }, options:{exp?:number,nbf?:number,iat?:number}) => {
-    return  await sign({payload,...options}, JWT_SECRET!, ALGO!);
- }
-
- export const verifyJwt = async (token:string) => {
-    try {
-        const payload = await verify(token, JWT_SECRET!, ALGO!);
-    } catch (error) {
-        
-    }
-    
- }
+const JWT_SECRET = Bun.env.JWT_SECRET;
+const ALGO = Bun.env.ALGORITHM as "HS256" | "HS384" | "HS512";
 
 
- import { decode } from 'hono/jwt'
+export const signJwt = async (
+  payload: { [key: string]: unknown },
+  options?: { exp?: number;}
+) => {
+  const now = new Date();
+  const expirationInSeconds = 60 * 60 * 2 * 7; //7 days expiration in seconds
+  const expirationTimestamp =
+    Math.floor(now.getTime() / 1000) + expirationInSeconds;
 
-// Decode the JWT token
+  return await sign(
+    { ...payload, exp:options?.exp || expirationTimestamp },
+    JWT_SECRET!,
+    ALGO!
+  );
+};
 
-
-
-const code = async () => {
-    const tokenToDecode =  await sign({user:{
-    name:"Sir",
-    email:"emailly@gmail.com",
-    id:"12345"}}, JWT_SECRET!, ALGO!);
-const anserr = await verify(tokenToDecode,JWT_SECRET!, ALGO!);
-const { header, payload } = decode(tokenToDecode);
-console.log('Decoded Header:', header);
-console.log('Decoded Payload:', anserr);
-}
-
-code();
+export const verifyJwt = async (token: string) => {
+  try {
+    const payload = await verify(token, JWT_SECRET!, ALGO!);
+  } catch (error) { return error; }
+};
