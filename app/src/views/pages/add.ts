d1112c1b58abe24bcb6e0c5fd3d9c5b4950de79f
Sir Guevara <groovyguev@gmail.com>
2024-02-20 12:44:22 -0600

    ok

--- app/src/views/pages/add.ts
+++ app/src/views/pages/add.ts
@@ -3,6 +3,7 @@ import { html } from "hono/html";
 const AddMonitorPage = ()=> html`
     <div class="container p-10 flex flex-col justify-center h-full">
         <h1 class="text-3xl text-center">Add Monitor Page</h1>
+        <p>Coming soon</p>
     </div>
 `;
 
